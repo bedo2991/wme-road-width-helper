7d0de679723cd19d492a395740768678c581aff7
bedo2991 <fra.bdn@gmail.com>
2021-09-01 00:00:32 +0200

    Add updateURL

--- header.js
+++ header.js
@@ -5,6 +5,7 @@
 // @description  Sets shortcuts for quickly updating road width data. Select a segment and use numbers to set the number of lanes.
 // @author       bedo2991 @ Waze
 // @require      https://craig.global.ssl.fastly.net/js/mousetrap/mousetrap.min.js
+// @updateURL	 https://github.com/bedo2991/wme-road-width-helper/releases/latest/download/release.user.js
 // @supportURL   https://www.waze.com/forum/ucp.php?i=pm&mode=compose&u=4945632
 // @include      /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
 // @require      https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
